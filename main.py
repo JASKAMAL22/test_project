05db975c3489000e40c19fe741b56dc9ecdafd12
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2023-10-27 10:42:36 +0530

    Update main.py
    added py source code

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-import logging
-import logging.handlers
-import os
+# example.py
 
+def msg(name):
+  print('welcome, {}!'.format(name))
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+if __name__ == '__main__':
+  msg(sys.argv[1])
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-
-
-
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-    print("CoudEQS!")
